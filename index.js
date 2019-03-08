a9178d92fd12abf3f37c3b9a953091a6827bba9b
F <F@F.F>
2019-03-08 16:42:30 +0800

    新增md5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require('./lib');
+const text = require('./lib/text');
+const crypto = require('./lib/crypto');
+const log = require('./lib/log');
+const data = require('./lib/data');
+const file = require('./lib/file');
+
+module.exports = {
+    text: text,
+    crypto: crypto,
+    log: log,
+    data: data,
+    file: file
+};
---
+++ test/data.test.js
@@ -0,0 +1,4 @@
+let utils = require('../index');
+
+console.log(utils.data.Format('yyyy年MM月dd日 hh:mm:ss S'));
+console.log(utils.data.Format('yy/MM/dd hh:mm:ss S'));
