85d339d173fa220604864133e02af01eb98242f8
F <F@F.F>
2019-03-14 13:19:31 +0800

    大修改，不能用

--- test/router.hello.test.js
+++
@@ -1,8 +0,0 @@
-const server = require('../lib/server');
-const router = server.router();
-
-router.get('/hello', function (req, res, next) {
-    console.log('\'/hello\', function (req, res, next)');
-})
-
-module.exports = router;
