65984e417c0cf775e73b65d9d9af49b726c47a67
f <f@f.f>
2019-03-15 21:49:08 +0800

    1.2.0稳定版

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const nlp = require('../nlp/index');
 const crypto = require('./lib/crypto');
 const date = require('./lib/date');
 const file = require('./lib/file');
@@ -6,7 +5,6 @@ const ip = require('./lib/ip');
 const text = require('./lib/text');
 
 module.exports = {
-    nlp: nlp,
     crypto: crypto,
     date: date,
     file: file,
